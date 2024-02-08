02bd17d53fea5b497dfeab8444ae535e2052c017
Shifu <aemre3113@gmail.com>
2024-02-08 16:14:56 +0300

    Kart oluşturma basitleştirildi. (Başlangıç Commiti)

--- src/App.js
+++ src/App.js
@@ -3,39 +3,33 @@ import Card from "./components/Card";
 import { useState } from "react";
 import { Button, Input, Textarea, Container, Stack, Grid } from "@mantine/core";
 
-let arr = [
-  {
-    title: "Dağ 1",
-    par: "açıklama 1",
-  },
-  {
-    title: "Dağ 2",
-    par: "açıklama 2",
-  },
-  {
-    title: "Dağ 3",
-    par: "açıklama 3",
-  },
-  {
-    title: "Dağ 4",
-    par: "açıklama 4",
-  },
-];
-
 const App = () => {
   const [title, setTitle] = useState("");
   const [paragraf, setParagraf] = useState("");
-  const [list, setList] = useState(arr);
-  const [lesson, setLesson] = useState([]);
+  const [list, setList] = useState([
+    {
+      title: "Dağ 1",
+      par: "açıklama 1",
+    },
+    {
+      title: "Dağ 2",
+      par: "açıklama 2",
+    },
+    {
+      title: "Dağ 3",
+      par: "açıklama 3",
+    },
+  ]);
   const click = () => {
     setTitle("");
     setParagraf("");
-    const copyList = [...list];
-    copyList.push({
-      title: title,
-      par: paragraf,
-    });
-    setList(copyList);
+    setList([
+      {
+        title: title,
+        par: paragraf,
+      },
+      ...list,
+    ]);
   };
   return (
     <Container>
@@ -62,7 +56,7 @@ const App = () => {
       <Grid>
         {list.map(({ title, par }, index) => (
           <Grid.Col span={4} key={index}>
-            <Card title={title} p={par} lesson={lesson} />
+            <Card title={title} p={par} />
           </Grid.Col>
         ))}
       </Grid>
